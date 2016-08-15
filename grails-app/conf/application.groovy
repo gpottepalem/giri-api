e39f09217bb7c89d0f64eca3303756100e1ca856
gpottepalem <gpottepalem@gsgenetics.com>
2016-08-14 22:25:38 -0400

    Added PostgreSQL and basic domain objects

--- grails-app/conf/application.groovy
+++ grails-app/conf/application.groovy
@@ -2,38 +2,38 @@
 grails.plugin.springsecurity.userLookup.userDomainClassName = 'com.giri.security.AppUser'
 grails.plugin.springsecurity.userLookup.authorityJoinClassName = 'com.giri.security.AppUserRole'
 grails.plugin.springsecurity.authority.className = 'com.giri.security.Role'
+
 grails.plugin.springsecurity.controllerAnnotations.staticRules = [
-    [pattern: '/',               access: ['permitAll']],
-    [pattern: '/error',          access: ['permitAll']],
-    [pattern: '/index',          access: ['permitAll']],
-    [pattern: '/index.gsp',      access: ['permitAll']],
-    [pattern: '/shutdown',       access: ['permitAll']],
-    [pattern: '/assets/**',      access: ['permitAll']],
-    [pattern: '/**/js/**',       access: ['permitAll']],
-    [pattern: '/**/css/**',      access: ['permitAll']],
-    [pattern: '/**/images/**',   access: ['permitAll']],
-    [pattern: '/**/favicon.ico', access: ['permitAll']],
+    [pattern: '/',                  access: ['permitAll']],
+    [pattern: '/error',             access: ['permitAll']],
+    [pattern: '/index',             access: ['permitAll']],
+    [pattern: '/index.gsp',         access: ['permitAll']],
+    [pattern: '/shutdown',          access: ['permitAll']],
+    [pattern: '/assets/**',         access: ['permitAll']],
+    [pattern: '/**/js/**',          access: ['permitAll']],
+    [pattern: '/**/css/**',         access: ['permitAll']],
+    [pattern: '/**/images/**',      access: ['permitAll']],
+    [pattern: '/**/favicon.ico',    access: ['permitAll']],
     //spring rest security api end-point
-    [pattern: '/api/logout',     access: ['isAuthenticated()']],
+    [pattern: '/api/logout',        access: ['isAuthenticated()']],
     //Spring boot Actuator management end-points
     [pattern: '/api/management/**', access:['ROLE_ADMIN']]
 ]
 
-grails.plugin.springsecurity.filterChain.chainMap = [
-    [pattern: '/assets/**',      filters: 'none'],
-    [pattern: '/**/js/**',       filters: 'none'],
-    [pattern: '/**/css/**',      filters: 'none'],
-    [pattern: '/**/images/**',   filters: 'none'],
-    [pattern: '/**/favicon.ico', filters: 'none'],
-    [pattern: '/**',             filters: 'JOINED_FILTERS']
-]
+//grails.plugin.springsecurity.filterChain.chainMap = [
+//    [pattern: '/assets/**',      filters: 'none'],
+//    [pattern: '/**/js/**',       filters: 'none'],
+//    [pattern: '/**/css/**',      filters: 'none'],
+//    [pattern: '/**/images/**',   filters: 'none'],
+//    [pattern: '/**/favicon.ico', filters: 'none'],
+//    [pattern: '/**',             filters: 'JOINED_FILTERS']
+//]
 
 grails.plugin.springsecurity.filterChain.chainMap = [
     //Stateless chain
-    [ pattern: '/api/**', filters: 'JOINED_FILTERS,-anonymousAuthenticationFilter,-exceptionTranslationFilter,-authenticationProcessingFilter,-securityContextPersistenceFilter,-rememberMeAuthenticationFilter'],
-
+    [ pattern: '/api/**', filters: 'JOINED_FILTERS,-exceptionTranslationFilter,-authenticationProcessingFilter,-securityContextPersistenceFilter,-rememberMeAuthenticationFilter'],
     //Traditional chain
-//    [ pattern: '/**', filters: 'JOINED_FILTERS,-restTokenValidationFilter,-restExceptionTranslationFilter']
+    //[ pattern: '/**', filters: 'JOINED_FILTERS,-restTokenValidationFilter,-restExceptionTranslationFilter']
 ]
 
 grails.plugin.springsecurity.rest.token.storage.useGorm = true
---
+++ grails-app/domain/com/giri/Specification.groovy
@@ -0,0 +1,12 @@
+package com.giri
+
+import grails.rest.*
+
+/**
+ * @author Gpottepalem
+ * Created on Aug 13, 2016
+ */
+@Resource(readOnly = false, formats = ['json', 'xml'])
+class Specification {
+
+}
